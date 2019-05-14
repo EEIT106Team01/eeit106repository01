24659c1d920fcc06ea1f9a8234b5778bab9b7c65
冒竣瑋 <EEIT-10633>
2019-05-14 11:12:37 +0800

    add ReviewBean MemberBeanTest relation.

--- eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/model/MemberBeanTest.java
+++ eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/model/MemberBeanTest.java
@@ -1,9 +1,14 @@
 package net.ddns.eeitdemo.eeit106team01.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import net.ddns.eeitdemo.eeit106team01.model.shop.product.ReviewBean;
 
 @Entity
 public class MemberBeanTest implements Serializable {
@@ -12,6 +17,10 @@ public class MemberBeanTest implements Serializable {
 
 	@Id
 	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "productbean")
+	List<ReviewBean> reviews = new ArrayList<ReviewBean>();
 
 	public Long getId() {
 		return id;
@@ -28,7 +37,5 @@ public class MemberBeanTest implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	private String name;
 
 }
