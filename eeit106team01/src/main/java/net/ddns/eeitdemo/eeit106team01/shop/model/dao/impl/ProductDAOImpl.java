c05dfd71c2eb97e7dfecf3b8b79d44573c2fe0bd
廖修汶 <wen@EEIT106>
2019-05-16 10:55:17 +0800

    add dao/impl

---
+++ eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/shop/model/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,77 @@
+package net.ddns.eeitdemo.eeit106team01.shop.model.dao.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import net.ddns.eeitdemo.eeit106team01.shop.model.ProductBean;
+import net.ddns.eeitdemo.eeit106team01.shop.model.dao.ProductDAO;
+
+public class ProductDAOImpl implements ProductDAO {
+
+	@Override
+	public ProductBean insertProduct(ProductBean productBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductBean updateProduct(ProductBean productBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductBean findProductByPrimaryKey(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductBean findProductBySerialNumber(String serialNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProductsByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProductsByBrand(String brand) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProductsByStock(Integer stock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProductsByType(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProductsByPrice(Integer minPrice, Integer maxPrice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductBean> findProductsByTime(Date time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
