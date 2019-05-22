93d880a91942b48f390dee9096b6bc94fd04ebef
廖修汶 <wen@EEIT106>
2019-05-22 14:16:51 +0800

    [SHOP] TopSearch Controller Service完成

--- eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/shop/model/dao/impl/TopSearchDAOImpl.java
+++ eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/shop/model/dao/impl/TopSearchDAOImpl.java
@@ -2,19 +2,14 @@ package net.ddns.eeitdemo.eeit106team01.shop.model.dao.impl;
 
 import java.util.List;
 
-import javax.persistence.Query;
-import javax.transaction.Transactional;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import net.ddns.eeitdemo.eeit106team01.shop.model.ProductBean;
 import net.ddns.eeitdemo.eeit106team01.shop.model.TopSearchBean;
 import net.ddns.eeitdemo.eeit106team01.shop.model.dao.TopSearchDAO;
 
-@Transactional
 @Repository
 public class TopSearchDAOImpl implements TopSearchDAO {
 
@@ -25,44 +20,11 @@ public class TopSearchDAOImpl implements TopSearchDAO {
 		return sessionFactory.getCurrentSession();
 	}
 	
-	
-	@SuppressWarnings("unchecked")
 	@Override
-	public TopSearchBean insertTopSearch(String keyWord) {
-		if (keyWord != null) {
-			Query query = this.getSession().createQuery("from TopSearchBean where keyWord = :keyWord", TopSearchBean.class);
-			query.setParameter("keyWord", keyWord);
-			List<TopSearchBean> keyWordResult = query.getResultList();
-			Query query1 = this.getSession().createQuery(
-					"from TopSearchBean where time >= DATEADD(day,:day ,GETDATE()) AND time <= DATEADD(day,:day1,GETDATE()) AND keyWord = :keyWord",
-					TopSearchBean.class);
-			query1.setParameter("day", -30);
-			query1.setParameter("day1", 0);
-			query1.setParameter("keyWord", keyWord);
-			List<TopSearchBean> timeResult =  query1.getResultList();
-			TopSearchBean topSearchBean = new TopSearchBean();
-			System.out.println(keyWordResult);
-			System.out.println(timeResult);
-			if (keyWordResult.size() == 0) { // keyWord不能重複
-				topSearchBean.setSearchTimes(1);
-				topSearchBean.setTime();
-				topSearchBean.setKeyWord(keyWord);
-				getSession().save(topSearchBean);
-				return topSearchBean;
-			}else if(keyWordResult.size() != 0 && timeResult.size() == 0 ) { //keyWord重複,但不是這個月內的
-				topSearchBean.setSearchTimes(1);
-				topSearchBean.setTime();
-				topSearchBean.setKeyWord(keyWord);
-				getSession().save(topSearchBean);
-				return topSearchBean;
-			}else if(keyWordResult.size() != 0 && timeResult.size() != 0) { //keyWord重複,且這個月內的,times+1
-				TopSearchBean result = keyWordResult.get(0);
-				result.setSearchTimes( (result.getSearchTimes() + 1 ) );
-				updateTopSearch(result);
-				return result;
-			}else {
-				return null;
-			}
+	public TopSearchBean insertTopSearch(TopSearchBean topSearchBean) {
+		if (topSearchBean != null) {
+			getSession().save(topSearchBean);
+			return topSearchBean;
 		}
 		return null;
 	}
