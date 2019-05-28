22d5223be764d3744ff74a74cf55e8de796e1a30
廖修汶 <wen@EEIT106>
2019-05-28 11:44:33 +0800

    [shop] search js

---
+++ eeit106team01/src/main/webapp/shop/js/search.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $("#searchPrice").click(function () {
+        getProductsByPrice();
+    });
+    getAllType();getAllBrand();
+})
+
+function getProductsByPrice(){
+    var price = $("#priceForm").serialize();
+    $.ajax({
+        url: "http://localhost:8080/search/price?" + price,
+        method: "GET",
+        dataType: "json",
+        success: function (priceData) {
+
+        },error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus)
+        }
+    })
+}
+
+function getAllBrand(){
+    
+    $.ajax({
+        url: "http://localhost:8080/search/type",
+        method: "GET",
+        dataType: "json",
+        success: function (brandData) {   
+            var i=0;    
+            var productBrandArray =[];
+            $.each(brandData,function(){
+                productBrandArray.push('<li><a href="#">'+brandData[i].brand+'</a></li>')
+                    i++
+                })   
+            $("#typeForm").append(productBrandArray.join(""))
+        },error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus)
+        }
+    })
+}
+
+function getAllType(){
+    
+    $.ajax({
+        url: "http://localhost:8080/search/brand",
+        method: "GET",
+        dataType: "json",
+        success: function (typeData) {   
+            var i=0;    
+            var productTypeArray =[];
+            $.each(typeData,function(){
+                    productTypeArray.push('<li><a href="#">'+typeData[i].type+'</a></li>')
+                    i++
+                })   
+            $("#typeForm").append(productTypeArray.join(""))
+        },error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus)
+        }
+    })
+}
