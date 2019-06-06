03c90bebde762bde8cd85ebc7d15ae14746f3d76
冒竣瑋 <EEIT-10633>
2019-06-06 09:02:36 +0800

    [shop] shopping-cart

--- eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/shop/model/dao/impl/ProductDAOImpl.java
+++ eeit106team01/src/main/java/net/ddns/eeitdemo/eeit106team01/shop/model/dao/impl/ProductDAOImpl.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -21,7 +19,6 @@ import net.ddns.eeitdemo.eeit106team01.shop.util.NullChecker;
 import net.ddns.eeitdemo.eeit106team01.shop.util.SerialNumberGenerator;
 
 @Repository
-@Transactional
 public class ProductDAOImpl implements ProductDAO {
 
 	@Autowired
--- eeit106team01/src/main/webapp/shop/js/shopping-cart.js
+++ eeit106team01/src/main/webapp/shop/js/shopping-cart.js
@@ -1,8 +1,8 @@
 //drag and drop event
 $("#draggable-events").draggable({
-  start: onDragStart,
-  drag: onDrag,
-  stop: onDragStop
+    start: onDragStart,
+    drag: onDrag,
+    stop: onDragStop
 });
 
 var $startCounter = $(".start-event");
@@ -10,181 +10,187 @@ var $dragCounter = $(".drag-event");
 var $stopCounter = $(".stop-event");
 
 var counts = {
-  start: 0,
-  drag: 0,
-  stop: 0
+    start: 0,
+    drag: 0,
+    stop: 0
 };
 
 function onDragStart() {
-  counts.start++;
-  updateCounter($startCounter, counts.start);
+    counts.start++;
+    updateCounter($startCounter, counts.start);
 }
 
 function onDrag() {
-  counts.drag++;
-  updateCounter($dragCounter, counts.drag);
+    counts.drag++;
+    updateCounter($dragCounter, counts.drag);
 }
 
 function onDragStop() {
-  counts.stop++;
-  updateCounter($stopCounter, counts.stop);
+    counts.stop++;
+    updateCounter($stopCounter, counts.stop);
 }
 
 function updateCounter(selector, value) {
-  $(selector).text(value);
+    $(selector).text(value);
 }
 
 //revert
 $("#draggable-events").draggable({
-  revert: true
+    revert: true
 });
 
 //Card Accordion
 //clicks
 $("#card-shopping-cart").click(function() {
-  openCardAccordion("shopping-cart");
+    openCardAccordion("shopping-cart");
 });
 
 function openCardAccordion(type) {
-  if (
-    type == "shopping-cart" &&
-    $("#accordion-shopping-cart img").attr("src") != undefined
-  ) {
-    enableSlideToggle(type);
-  }
+    if (
+        type == "shopping-cart" &&
+        $("#accordion-shopping-cart img").attr("src") != undefined
+    ) {
+        enableSlideToggle(type);
+    }
 }
 
 //enableSlideToggle
 function enableSlideToggle(type) {
-  if (type == "shopping-cart") {
-    $("#accordion-shopping-cart").slideToggle("slow", "linear");
-  }
+    if (type == "shopping-cart") {
+        $("#accordion-shopping-cart").slideToggle("fast", "linear");
+    }
 }
 
 //mouseover event
 $("#card-shopping-cart").mouseover(function() {
-  $("#card-shopping-cart .card-img-top").attr(
-    "class",
-    "card-img-top mr-1 mouseover"
-  );
+    $("#card-shopping-cart .card-img-top").attr(
+        "class",
+        "card-img-top mr-1 mouseover"
+    );
 });
 $("#card-shopping-cart").mouseout(function() {
-  $("#card-shopping-cart .card-img-top").attr("class", "card-img-top mr-1");
+    $("#card-shopping-cart .card-img-top").attr("class", "card-img-top mr-1");
 });
 
 //get all from cart(localStorage)
 function allStorage() {
-  var values = [],
-    keys = Object.keys(localStorage),
-    i = keys.length;
-  while (i--) {
-    values.push(localStorage.getItem(keys[i]));
-  }
-  return values;
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+    while (i--) {
+        values.push(localStorage.getItem(keys[i]));
+    }
+    return values;
 }
 
 //add to cart(localStorage)
 function addToCart() {
-  var cartLocalStorage = localStorage;
-  var cartLocalStorageCount = localStorage.length;
+    var cartLocalStorage = localStorage;
+    var cartLocalStorageCount = localStorage.length;
 
-  $(document).ready(function() {
-    $("#card-shopping-cart-count").text(cartLocalStorageCount);
-    if (cartLocalStorageCount > 0) {
-      var jsonArray = [];
-      allStorage().forEach(element => {
-        var result = JSON.parse(element);
-        jsonArray.push(result);
-      });
+    $(document).ready(function() {
+        $("#card-shopping-cart-count").text(cartLocalStorageCount);
+        if (cartLocalStorageCount > 0) {
+            var jsonArray = [];
+            allStorage().forEach(element => {
+                var result = JSON.parse(element);
+                jsonArray.push(result);
+            });
 
-      //clear
-      deletePurchase = function(data) {
-        $(".deletePurchase").ready(function() {
-          cartLocalStorage.removeItem(data.id);
-          location.href = location.href;
-        });
-      };
+            //clear
+            deletePurchase = function(data) {
+                $(".deletePurchase").ready(function() {
+                    cartLocalStorage.removeItem(data.id);
+                    location.href = location.href;
+                });
+            };
 
-      // var purchaseButton = `<button type="button" class="btn btn-link"><span class='fa fa-shopping-cart'> 去結帳</span></button>`;
+            let purchaseButton = `<button type="button" class="btn btn-block"><span class='fa fa-shopping-cart'> 去結帳</span></button>`;
+            let productUrl = urlDomain + `shop/product.html?`;
 
-      jsonArray.forEach(element => {
-        var text = [];
-        var closeIcon = [];
-        var img = [];
-        $.each(element, function(key, val) {
-          if (key.match(/^(name)$/) && val != null) {
-            text.push("<span>品名: " + val.substr(0, 10) + "</span></br>");
-          } else if (key.match(/^(price)$/)) {
-            text.push("<span>價錢: " + "$" + val + "</span></br>");
-          } else if (key.match(/^(quantity)$/)) {
-            text.push("<span>數量: " + val + "</span>");
-          } else if (key.match(/^(id)$/)) {
-            text.push(
-              "<span class='hidden' id='" + val + "'>" + val + "</span>"
-            );
-            closeIcon.push(
-              "<span class='badge' onclick='deletePurchase(this)' id='" +
-                val +
-                "'><span class='fa fa-close'></span></span>"
-            );
-          } else if (key.match(/^(image)$/) && val != null) {
-            img.push("<img src='" + val + "'>");
-          }
-        });
-        $(
-          `<span class='row cart-product'>` +
-            `<span class='col-sm-4'>` +
-            closeIcon.join("") +
-            img.join("") +
-            `</span>` +
-            text.join("") +
-            `</span>`
-        ).appendTo("#accordion-shopping-cart .scrollable");
-      });
-      // $(purchaseButton).appendTo("#accordion-shopping-cart");
-    }
-  });
+            jsonArray.forEach(element => {
+                var text = [];
+                var closeIcon = [];
+                var img = [];
+                var id = [];
+                $.each(element, function(key, val) {
 
-  $("#shoppingCartButton").click(function() {
-    var productURL = location.href;
-    var productId = parseInt(
-      productURL.substr(productURL.indexOf("?") + 1, productURL.length)
-    );
-    var productName = $("#productName").text();
-    var productPrice = parseInt(
-      $("#productPrice")
-        .text()
-        .substr(
-          $("#productPrice")
+                    if (key.match(/^(name)$/) && val != null) {
+                        text.push("<span>品名: " + val.substr(0, 6) + "...</span></br>");
+                    } else if (key.match(/^(price)$/)) {
+                        text.push("<span>價錢: " + "$" + val + "</span></br>");
+                    } else if (key.match(/^(quantity)$/)) {
+                        text.push("<span>數量: " + val + "</span>");
+                    } else if (key.match(/^(id)$/)) {
+                        id.push(val);
+                        text.push(
+                            "<span class='hidden' id='" + val + "'>" + val + "</span>"
+                        );
+                        closeIcon.push(
+                            "<span class='badge' onclick='deletePurchase(this)' id='" +
+                            val +
+                            "'><span class='fa fa-close'></span></span>"
+                        );
+                    } else if (key.match(/^(image)$/) && val != null) {
+                        img.push("<img src='" + val + "'>");
+                    }
+                });
+                $(`<a href="` + productUrl + id.join("") + `">` +
+                    `<span class='row cart-product'>` +
+                    `<span class='col-sm-4'>` +
+                    closeIcon.join("") +
+                    img.join("") +
+                    `</span>` +
+                    text.join("") +
+                    `</span>` +
+                    `</a>`
+                ).appendTo("#accordion-shopping-cart .scrollable");
+            });
+
+            $(purchaseButton).appendTo("#accordion-shopping-cart .scrollable");
+        }
+    });
+
+    $("#shoppingCartButton").click(function() {
+        var productURL = location.href;
+        var productId = parseInt(
+            productURL.substr(productURL.indexOf("?") + 1, productURL.length)
+        );
+        var productName = $("#productName").text();
+        var productPrice = parseInt(
+            $("#productPrice")
             .text()
-            .indexOf("$") + 1,
-          $("#productPrice").text().length
-        )
-    );
-    var productQuantity = parseInt($("#quantity option:selected").val());
-    var productImage = $("#productLeftInfo img").attr("src");
+            .substr(
+                $("#productPrice")
+                .text()
+                .indexOf("$") + 1,
+                $("#productPrice").text().length
+            )
+        );
+        var productQuantity = parseInt($("#quantity option:selected").val());
+        var productImage = $("#productLeftInfo img").attr("src");
 
-    //create json object
-    var productInfoJson = new Object();
-    productInfoJson.id = productId;
-    productInfoJson.name = productName;
-    productInfoJson.price = productPrice;
-    productInfoJson.quantity = productQuantity;
-    productInfoJson.image = productImage;
-    var productData = JSON.stringify(productInfoJson);
+        //create json object
+        var productInfoJson = new Object();
+        productInfoJson.id = productId;
+        productInfoJson.name = productName;
+        productInfoJson.price = productPrice;
+        productInfoJson.quantity = productQuantity;
+        productInfoJson.image = productImage;
+        var productData = JSON.stringify(productInfoJson);
 
-    if (null != cartLocalStorage.getItem(productId)) {
-      cartLocalStorage.removeItem(cartLocalStorage.getItem(productId));
-      cartLocalStorage.setItem(productId, productData);
-    } else {
-      cartLocalStorage.setItem(productId, productData);
-    }
+        if (null != cartLocalStorage.getItem(productId)) {
+            cartLocalStorage.removeItem(cartLocalStorage.getItem(productId));
+            cartLocalStorage.setItem(productId, productData);
+        } else {
+            cartLocalStorage.setItem(productId, productData);
+        }
 
-    var result = cartLocalStorage.getItem(productId);
-  });
+        var result = cartLocalStorage.getItem(productId);
+    });
 
-  //button click reload
-  $("#shoppingCartButton").click(function() {
-    location.href = location.href;
-  });
+    //button click reload
+    $("#shoppingCartButton").click(function() {
+        location.href = location.href;
+    });
 }
