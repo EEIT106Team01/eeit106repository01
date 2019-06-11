448321d6dbac6449116159dcba1ca2001aa065d6
冒竣瑋 <EEIT-10633>
2019-06-11 15:05:51 +0800

    [shop]purchase-show.html

--- eeit106team01/src/main/webapp/shop/js/util.js
+++ eeit106team01/src/main/webapp/shop/js/util.js
@@ -32,3 +32,13 @@ function findAllFromLocalStorage() {
     };
     return jsonArray;
 };
+
+//計算時間
+function GetDateStr(AddDayCount) {
+    var dd = new Date();
+    dd.setDate(dd.getDate() + AddDayCount);
+    var y = dd.getFullYear();
+    var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1);
+    var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate();
+    return y + "-" + m + "-" + d;
+}
